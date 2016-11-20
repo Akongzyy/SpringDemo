69ba47e0108852de5f751fa9b2e6a316274a1e42
zhangyuyang <1064050644@qq.com>
2016-11-20 15:04:25 +0800

    my first bean1

---
+++ src/main/java/com/gdei/Animal.java
@@ -0,0 +1,41 @@
+package com.gdei;
+
+
+public class Animal {
+    private String name;
+    private int size;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/main/java/com/gdei/Main.java
+++ src/main/java/com/gdei/Main.java
@@ -10,5 +10,9 @@ public class Main {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("carbean.xml");
         Car car = (Car) applicationContext.getBean("baoma");
         System.out.println(car);
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("animal.xml");
+        Animal animal = (Animal) ctx.getBean("animal1");
+        System.out.println(animal);
     }
 }
